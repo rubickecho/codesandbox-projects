1d5ddfe0e9d603f65b1faa249603496624bd4859
peakcool <tangliangcheng852@gmail.com>
2022-12-19 08:46:24 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 function Example() {
   const [count, setCount] = useState(0);
+  const latestCount = useRef(count);
 
   useEffect(() => {
+    latestCount.current = count;
+
     setTimeout(() => {
-      console.log(`You clicked ${count} times`);
+      console.log(`You clicked ${latestCount.current} times`);
     }, 3000);
   });
 
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
